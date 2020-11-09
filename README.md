be5107b790c04e15b45af9bb046a2b15b98afa97
Jocelle Elvambuena <71588038+jocelle23@users.noreply.github.com>
2020-11-09 00:54:41 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# github-api
+
+[Github API - List User's Respositories](https://jocelle23.github.io/github-api-assignment/)
+
+Assignment:
+"Your team is working on an app that will help recruiters review all of a candidate's information. You've been assigned to work on one feature for the app - to display a list of repos belonging to a particular GitHub handle."
---
+++ index.js
@@ -0,0 +1,41 @@
+
+function displayResults(responseJson) {
+    // if there are previous results, remove them
+    $('#user-repos').removeClass('hidden');
+    console.log(responseJson);
+    $('#user-repos-list').empty();
+
+    // iterate through the articles array, stopping at the max number of results
+    for (let i = 0; i < responseJson.length; i++) {
+        $('#user-repos-list').append(`
+        <li><h3><a href="${responseJson[i].html_url}">${responseJson[i].name}</a></h3></li>
+        `
+        )
+    };
+};
+
+function listUserRepo(userHandle) {
+    const searchURL = `https://api.github.com/users/${userHandle}/repos`;
+
+    fetch(searchURL)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+}
+
+function watchForm() {
+    $('#js-form').submit(event => {
+        event.preventDefault();
+        let userHandle = $("#js-search-term").val();
+        listUserRepo(userHandle);
+    });
+}
+
+$(watchForm);
